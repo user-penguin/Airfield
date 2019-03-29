f420f2cc4fe008dc164abe7446622f9b6b8250ba
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-03-29 16:59:06 +0700

    Тест на создание и получение RealX

--- src/main/java/model/Point.java
+++ src/main/java/model/Point.java
@@ -1,14 +1,18 @@
 package model;
 
 public class Point {
+
+    private double x;
+
     public Point(double x, double y) {
+        this.x = x;
     }
 
     public Point(int x, int y) {
     }
 
     public double getRealX() {
-        return 0;
+        return x;
     }
 
     public double getRealY() {
--- src/test/java/model/PointTest.java
+++ src/test/java/model/PointTest.java
@@ -18,4 +18,10 @@ class PointTest {
         assertEquals(0.0, point.getPxX());
         assertEquals(0.0, point.getPxY());
     }
+
+    @Test
+    void GetRealPointTest() {
+        Point point = new Point(10.0, 0.0);
+        assertEquals(10.0, point.getRealX());
+    }
 }
