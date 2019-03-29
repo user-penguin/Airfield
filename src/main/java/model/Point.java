4dd6f188919d518f51e4cfc660e72b7a432c669d
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-03-29 17:18:14 +0700

    Тест преобразование координат в конструкторе

--- src/main/java/model/Point.java
+++ src/main/java/model/Point.java
@@ -2,6 +2,11 @@ package model;
 
 public class Point {
 
+    private int PX_WIDTH = 1366; // в пикселях
+    private int PX_HEIGHT = 592; // в пикселях
+    private double REAL_WIDTH = 10_000.0; // в метрах
+    private double REAL_HEIGHT = 5_000.0; // в метрах
+
     private double x;
     private double y;
 
@@ -11,6 +16,8 @@ public class Point {
     }
 
     public Point(int x, int y) {
+        this.x = (double) x / PX_WIDTH * REAL_WIDTH;
+        this.y = (double) y / PX_HEIGHT * REAL_HEIGHT;
     }
 
     public double getRealX() {
@@ -21,11 +28,11 @@ public class Point {
         return y;
     }
 
-    public double getPxX() {
-        return 0;
+    public int getPxX() {
+        return (int) (x / REAL_WIDTH * PX_WIDTH);
     }
 
-    public double getPxY() {
-        return 0;
+    public int getPxY() {
+        return (int) (y / REAL_HEIGHT * PX_HEIGHT);
     }
 }
--- src/test/java/model/PointTest.java
+++ src/test/java/model/PointTest.java
@@ -5,29 +5,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
+
+    private int PX_DELTA = 5;
+    private double REAL_DELTA = 5.0;
+
     @Test
     void CreateRealPointTest() {
         Point point = new Point(0.0, 0.0);
-        assertEquals(0.0, point.getRealX());
-        assertEquals(0.0, point.getRealY());
     }
 
     @Test
     void CreatePxPointTest() {
         Point point = new Point(0, 0);
-        assertEquals(0.0, point.getPxX());
-        assertEquals(0.0, point.getPxY());
     }
 
     @Test
-    void GetRealXPointTest() {
+    void GetRealXFromRealPointTest() {
         Point point = new Point(10.0, 0.0);
         assertEquals(10.0, point.getRealX());
     }
 
     @Test
-    void GetRealYPointTest() {
+    void GetRealYFromRealPointTest() {
         Point point = new Point(0.0, 10.0);
         assertEquals(10.0, point.getRealY());
+    }
+
+    @Test
+    void GetPxXFromRealPointTest() {
+        Point point = new Point(10_000.0, 0.0);
+        assertEquals(1366, point.getPxX(), PX_DELTA);
+    }
+
+    @Test
+    void GetPxYFromRealPointTest() {
+        Point point = new Point(0.0, 5_000.0);
+        assertEquals(592, point.getPxY(), PX_DELTA);
+    }
+
+    @Test
+    void GetRealXFromPxPointTest() {
+        Point point = new Point(1366, 0);
+        assertEquals(10_000.0, point.getRealX(), REAL_DELTA);
+    }
+
+    @Test
+    void GetRealYFromPxPointTest() {
+        Point point = new Point(0, 592);
+        assertEquals(5_000.0, point.getRealY(), REAL_DELTA);
     }
 }
