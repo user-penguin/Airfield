cd345cc66c517bfc7d9781f8eddce220d84a394f
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-03-29 16:54:47 +0700

    Тест на получение реальных координат

--- src/main/java/model/Point.java
+++ src/main/java/model/Point.java
@@ -3,4 +3,12 @@ package model;
 public class Point {
     public Point(double x, double y) {
     }
+
+    public double getRealX() {
+        return 0;
+    }
+
+    public double getRealY() {
+        return 0;
+    }
 }
--- src/test/java/model/PointTest.java
+++ src/test/java/model/PointTest.java
@@ -8,5 +8,7 @@ class PointTest {
     @Test
     void CreateRealPointTest() {
         Point point = new Point(0.0, 0.0);
+        assertEquals(0.0, point.getRealX());
+        assertEquals(0.0, point.getRealY());
     }
 }
