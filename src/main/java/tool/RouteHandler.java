85fbc3cc2c4b0677309abae83e035f0f3151d16f
user-penguin <kobzev96@gmail.com>
2019-03-31 04:49:25 +0700

    add reed coordinates with speed

--- src/main/java/tool/RouteHandler.java
+++ src/main/java/tool/RouteHandler.java
@@ -48,4 +48,27 @@ public class RouteHandler {
         }
         return route;
     }
+
+    public static Route readRouteWithSpeed (String filename) {
+        // маршруты лежат в папке way
+        String pathToSource = "/way/" + filename + ".txt";
+        Route route = new Route();
+        try {
+            FileInputStream fstream = new FileInputStream(RouteHandler.class.getResource(pathToSource).getPath());
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String read;
+            while((read = br.readLine()) != null) {
+                String[] xyFromFile = read.split(" ");
+                int xCoordinate = Integer.parseInt(xyFromFile[0]);
+                int yCoordinate = Integer.parseInt(xyFromFile[1]);
+                double zCoordinate = Double.parseDouble(xyFromFile[2]);
+                double speed = Double.parseDouble(xyFromFile[3]);
+                Point point = new Point(xCoordinate, yCoordinate, zCoordinate, speed);
+                route.addCoordinate(point);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return route;
+    }
 }
